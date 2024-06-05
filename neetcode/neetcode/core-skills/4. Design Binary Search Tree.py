796440b54ed2df957c112d8c60b818351cbecdfd
asif <mahfujasif@gmail.com>
2024-06-05 22:04:43 +0100

    2 3 4 core skill

---
+++ neetcode/neetcode/core-skills/4. Design Binary Search Tree.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, value: int, ):
+        self.v
+
+class TreeMap:
+
+    def __init__(self):
+
+    def insert(self, key: int, val: int) -> None:
+
+    def get(self, key: int) -> int:
+
+    def getMin(self) -> int:
+
+    def getMax(self) -> int:
+
+    def remove(self, key: int) -> None:
+
+    def getInorderKeys(self) -> List[int]:
+
