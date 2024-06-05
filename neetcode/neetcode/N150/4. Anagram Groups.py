ee422c69e5827d2cebdabe3c4184540ed4b5aea7
asif <mahfujasif@gmail.com>
2024-06-06 00:27:21 +0100

    4 v2

--- neetcode/neetcode/N150/4. Anagram Groups.py
+++ neetcode/neetcode/N150/4. Anagram Groups.py
@@ -1,8 +1,20 @@
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
+
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            result[tuple(count)].append(s)
+        return result.values()
+
+    def groupAnagrams2(self, strs: List[str]) -> List[List[str]]:
         result = dict()
 
         for mem in strs:
